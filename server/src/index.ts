407d42470f63829fc4a628e7d9064a8e777a98de
Mohd_Hassan_Ashraf <hassanashraf32553@gmail.com>
2025-07-17 00:27:42 +0530

    Update CORS to allow frontend access

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,16 +5,28 @@ dotenv.config()
 import express from "express"
 import cors from "cors"
 
+
 import { ConnectionDb } from "./config"
 import { userRouter } from "./routes/userRouter"
 import { paymentRouter } from "./routes/paymentRouter"
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://walletpay-frontend1.onrender.com"
+];
+
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const MONGODB_URL = process.env.MONGODB_URL as string
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(cors());
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 app.use("/api/v1",userRouter)
 app.use("/api/v1",paymentRouter)
